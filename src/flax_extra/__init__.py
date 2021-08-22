f8700f5ff30eca0b58510fe084e73a8067ecddeb
Andrei Nesterov <ae.nesterov@gmail.com>
2021-08-23 02:37:58 +0300

    Specify top level interface for the package

--- src/flax_extra/__init__.py
+++ src/flax_extra/__init__.py
@@ -1,1 +1,27 @@
 """Flax extras."""
+
+from flax_extra import (
+    util,
+    random,
+    console,
+    batch,
+    operator,
+    layer,
+    model,
+    training,
+    evaluation,
+    checkpoint,
+)
+
+__all__ = [
+    "util",
+    "random",
+    "console",
+    "batch",
+    "operator",
+    "layer",
+    "model",
+    "training",
+    "evaluation",
+    "checkpoint",
+]
