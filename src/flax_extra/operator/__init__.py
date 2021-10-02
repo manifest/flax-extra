d184f586db17b48c88fa9ad0d16e76a9e2cfb3c7
Andrei Nesterov <ae.nesterov@gmail.com>
2021-10-02 21:24:10 +0300

    Add Rearrange operator

--- src/flax_extra/operator/__init__.py
+++ src/flax_extra/operator/__init__.py
@@ -1,11 +1,13 @@
 """Operator is just a function."""
 
 from redex.operator import add, sub, identity
+from flax_extra.operator._rearrange import Rearrange
 from flax_extra.operator._shift_right import ShiftRight
 
 __all__ = [
     "add",
     "sub",
     "identity",
+    "Rearrange",
     "ShiftRight",
 ]
