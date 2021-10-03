6e66baf7c105f50efb48e85ebbef5b264601889c
Andrei Nesterov <ae.nesterov@gmail.com>
2021-10-03 13:23:48 +0300

    Allow interlinks to Redex

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Looks for objects in external projects.
+intersphinx_mapping = {
+    "redex": ("https://redex.readthedocs.io/en/latest", None),
+}
+
 
 # -- Options for HTML output -------------------------------------------------
 
