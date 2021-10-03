1d4acc0d01a8dfec9c19108a73b7df12611fc57a
Andrei Nesterov <ae.nesterov@gmail.com>
2021-10-03 23:52:26 +0300

    Add layers of the Perceiver model

--- src/flax_extra/model/__init__.py
+++ src/flax_extra/model/__init__.py
@@ -1,7 +1,11 @@
 """Flax models."""
 
 from flax_extra.model._rnn import RNNLM
+from flax_extra.model._preceiver import PerceiverEncoder, PerceiverDecoder, PerceiverProcessor
 
 __all__ = [
     "RNNLM",
+    "PerceiverEncoder",
+    "PerceiverDecoder",
+    "PerceiverProcessor",
 ]
