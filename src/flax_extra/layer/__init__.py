8196526ed9d04775a891cd1a712c23eb9f4bf98d
Andrei Nesterov <ae.nesterov@gmail.com>
2021-10-03 14:47:09 +0300

    Add Fourier positional encoding

--- src/flax_extra/layer/__init__.py
+++ src/flax_extra/layer/__init__.py
@@ -2,9 +2,13 @@
 
 from flax_extra.layer._lstm import LSTM, LSTMCell
 from flax_extra.layer._feedforward import FeedForward
+from flax_extra.layer._fourier_positional_encoding import FourierPositionEncoding
+from flax_extra.layer._trainable_positional_encoding import TrainablePositionalEncoding
 
 __all__ = [
     "LSTM",
     "LSTMCell",
     "FeedForward",
+    "FourierPositionEncoding",
+    "TrainablePositionalEncoding",
 ]
