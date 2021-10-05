450623bf38fbcc975384fe89c9c9a6148503c60d
Andrei Nesterov <ae.nesterov@gmail.com>
2021-10-05 23:58:09 +0300

    Add positional encoding, masking, and padding

---
+++ src/flax_extra/layer/_trainable_positional_masking.py
@@ -0,0 +1,56 @@
+r"""Trainable positional maskin."""
+
+import jax
+from jax import numpy as jnp
+from flax import linen as nn
+from flax_extra.layer._trainable_positional_encoding import TrainablePositionalEncoding
+
+Array = jnp.ndarray
+
+
+class TrainablePositionalMasking(nn.Module):
+    r"""Trainable positional masking.
+
+    Learns a mask vector and uses it to mask a vector at each position
+    (i.e. time step) of the sequence according specified probabilistic rate.
+
+    .. math::
+
+        \begin{aligned}
+            & \textrm{TrainablePositionalMasking}( \\
+            & \quad x \in \sR^{\nBatchSize \times T \times d} \\
+            & \quad \_ \\
+            & \quad w \in \sR^{1 \times d} \\
+            & ) \\
+            & \rightarrow \sR^{\nBatchSize \times T \times d}
+        \end{aligned}
+    """
+
+    rate: float
+    r"""a probability for a vector at each position being masked."""
+
+    @nn.compact
+    def __call__(self, inputs: Array) -> Array:  # type: ignore[override] # pylint: disable=arguments-differ
+        r"""Applies a mask to the sequence.
+
+        Args:
+            inputs: a sequence.
+
+        Returns:
+            a masked sequence.
+        """
+        batch_size, seqlen_input, d_input = inputs.shape
+        masking = TrainablePositionalEncoding(
+            seqlen=1,
+            dimension=d_input,
+        )
+
+        mask_weight = jax.random.bernoulli(
+            self.make_rng("params"),
+            self.rate,
+            shape=(batch_size, seqlen_input),
+        )[:, :, jnp.newaxis]
+
+        mask_token = masking(batch_size=batch_size)
+        masked_inputs = (1 - mask_weight) * inputs + mask_weight * mask_token
+        return masked_inputs  # type: ignore
---
+++ src/flax_extra/layer/_trainable_positional_padding.py
@@ -0,0 +1,62 @@
+r"""Trainable positional padding."""
+
+from typing import List
+from jax import numpy as jnp
+from flax import linen as nn
+from flax_extra.layer._trainable_positional_encoding import TrainablePositionalEncoding
+
+Array = jnp.ndarray
+Positions = List[int]
+
+
+class TrainablePositionalPadding(nn.Module):
+    r"""Trainable positional padding.
+
+    Learns a padding vector and uses it to pad a vector at each position
+    (i.e. time step) of the sequence to desired dimension.
+
+    .. math::
+
+        \begin{aligned}
+            & \textrm{TrainablePositionalPadding}( \\
+            & \quad x \in \sR^{\nBatchSize \times T \times d} \\
+            & \quad \_ \\
+            & \quad w \in  \sR^{1 \times d^{\prime}} \\
+            & ) \\
+            & \rightarrow \sR^{\nBatchSize \times T \times d + d^{\prime}}
+        \end{aligned}
+    """
+
+    d_max: int
+    r"""a desired dimension for vectors in the sequence."""
+
+    @nn.compact
+    def __call__(self, inputs: Array) -> Array:  # type: ignore[override] # pylint: disable=arguments-differ
+        r"""Adds padding to the sequence.
+
+        Args:
+            inputs: a sequence.
+
+        Returns:
+            a sequence with padding along feature dimension.
+        """
+        batch_size = inputs.shape[0]
+        d_input = inputs.shape[-1]
+        d_padding = self.d_max - d_input
+        padding = TrainablePositionalEncoding(
+            seqlen=1,
+            dimension=d_padding,
+        )
+
+        padded_input_shape = list(inputs.shape)
+        padded_input_shape[-1] = d_padding
+        pad_tokens = jnp.broadcast_to(
+            padding(batch_size=batch_size),
+            shape=padded_input_shape,
+        )
+
+        padded_inputs = jnp.concatenate(
+            [inputs, pad_tokens],
+            axis=-1,
+        )
+        return padded_inputs  # type: ignore
