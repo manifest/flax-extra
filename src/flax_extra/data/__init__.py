a4ce61c2a2cd95052ac02d26d98a1fb002c25350
Andrei Nesterov <ae.nesterov@gmail.com>
2021-10-07 20:52:51 +0300

    Add bytes tokenizer

---
+++ src/flax_extra/data/__init__.py
@@ -0,0 +1,8 @@
+r"""Data processing modules and functions."""
+
+from flax_extra.data._bytes_tokenizer import BytesTokenizer, bytes_tokenizer
+
+__all__ = [
+    "BytesTokenizer",
+    "bytes_tokenizer",
+]
